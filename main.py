7fcf6cbb2603243b4ae6fa8ac9f7a2f1ab986333
6134ataalpsahinoglu <atamatayap@gmail.com>
2025-04-18 17:35:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import random
 anilarim=["bir kez valorant'ta ilk raunda bela atmıştım","gta5 te çok değişik bi hareket yapmıştım motor la","çizim tabletim kırıldı"]
 print(random.choice(anilarim))
-print(ataalp sekme)
+
